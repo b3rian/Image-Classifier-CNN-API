9782882e059037712bb3d908d13a36db1982ece3
b3rian <brayann.8189@gmail.com>
2025-07-06 13:27:14 +0300

    edited

--- train.py
+++ train.py
@@ -1,17 +1,17 @@
 from trainers.trainer import Trainer
-from models.resnet import build_resnet18
+from models import build_resnet
 from utils.logger import get_callbacks
-from input_pipeline import prepare_datasets
+from data.input_pipeline import get_datasets
 from utils.seed import set_seed
 import yaml
 
 config = yaml.safe_load(open("configs/resnet.yml"))
 set_seed(config["seed"])
 
-train_ds, val_ds = prepare_datasets(config)
+train_ds, val_ds = get_datasets(config)
 
 trainer = Trainer(
-    model_fn=build_resnet18,
+    model_fn=build_resnet,
     train_ds=train_ds,
     val_ds=val_ds,
     config=config,
