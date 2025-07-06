68d02a196518c4bc6e85ca7eaf63c8f4bfd33148
b3rian <brayann.8189@gmail.com>
2025-07-06 15:58:54 +0300

    edited

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -2,5 +2,7 @@ import tensorflow as tf
 
 def get_model_preprocessing_layer():
     return tf.keras.Sequential([
-        tf.keras.layers.Rescaling(1./255)  # Normalize pixels to [0, 1]
+        tf.keras.layers.Rescaling(1./255),# Normalize pixel values to [0, 1]
+        tf.keras.layers.Normalization(mean=[0.485, 0.456, 0.406],
+                                       variance=[0.229**2, 0.224**2, 0.225**2]) # Normalize using ImageNet statistics 
     ])
