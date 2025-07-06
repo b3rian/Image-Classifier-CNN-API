cfc7da451d4624e58bfee22b7758c1b60a2992b8
b3rian <brayann.8189@gmail.com>
2025-07-06 14:04:36 +0300

    edited

--- eval.py
+++ eval.py
@@ -1,22 +1,25 @@
 import tensorflow as tf
 from metrics.metrics import get_classification_metrics
-from input_pipeline import prepare_datasets
+from data.input_pipeline import get_datasets
 import yaml
 import argparse
 import os
 
 def evaluate(model_path, config):
-    _, val_ds = prepare_datasets(config)
+     # Load test dataset (ignore train_ds and val_ds with `_, _`)
+    _, _, test_ds = get_datasets(config)
 
+    # load saved model without compiling
     model = tf.keras.models.load_model(model_path, compile=False)
 
+    # recompile the model with the correct loss and metrics
     model.compile(
         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
         metrics=get_classification_metrics()
     )
-
-    results = model.evaluate(val_ds, return_dict=True)
-    print(f"/n[RESULTS for {os.path.basename(model_path)}]")
+    # evaluate on the test dataset
+    results = model.evaluate(test_ds, return_dict=True)
+    print(f"/n[TEST RESULTS for {os.path.basename(model_path)}]")
     for k, v in results.items():
         print(f"{k}: {v:.4f}")
 
