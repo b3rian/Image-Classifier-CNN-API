9d9190425c6d33324a9d8ac269c094759103575e
b3rian <brayann.8189@gmail.com>
2025-07-07 16:30:51 +0300

    edited

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 def get_model_preprocessing_layer():
     return tf.keras.Sequential([
-        tf.keras.layers.Resizing(254, 254),
+        tf.keras.layers.Resizing(224, 224),
         tf.keras.layers.Lambda(
             lambda img: tf.clip_by_value(img, 0.0, 255.0), name="clip_pixels"),
         tf.keras.layers.Rescaling(1./255),# Normalize pixel values to [0, 1]
