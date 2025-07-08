94ac75375ccad6d7be4f1f859e483ec37b739f2e
b3rian <brayann.8189@gmail.com>
2025-07-08 10:22:49 +0300

    edited

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -5,6 +5,5 @@ def get_model_preprocessing_layer():
         tf.keras.layers.Resizing(224, 224),
         tf.keras.layers.Lambda(
             lambda img: tf.clip_by_value(img, 0.0, 255.0), name="clip_pixels"),
-        tf.keras.layers.Rescaling(1./255),# Normalize pixel values to [0, 1]
-],
-        name="preprocessing_pipeline") # Normalize using ImageNet statistics ))
+    tf.keras.layers.Rescaling(1./255)  # Normalize pixel values to [0, 1]
+],  name="preprocessing_pipeline")  # Normalize using ImageNet statistics
