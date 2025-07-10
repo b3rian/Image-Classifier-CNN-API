988a60b316c5c114c22e7af729ab9a5412b37605
b3rian <brayann.8189@gmail.com>
2025-07-10 14:08:29 +0300

    edited

--- models/build_resnet.py
+++ models/build_resnet.py
@@ -7,21 +7,7 @@ def make_simple_cnn(input_shape=(64, 64, 3), num_classes=200):
         layers.Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=input_shape),
         layers.MaxPooling2D((2, 2)),
 
-        # Block 2
-        layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
-        layers.MaxPooling2D((2, 2)),
-
-        # Block 3
-        layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
-        layers.MaxPooling2D((2, 2)),
-
-        # Head
-        layers.Flatten(),
-        layers.Dense(256, activation='relu'),
-        layers.Dropout(0.5),
-        layers.Dense(num_classes, activation='softmax')
-    ])
-    return model
+       
 
       
 
