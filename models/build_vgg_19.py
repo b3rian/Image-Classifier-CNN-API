594e16173b066cf031ac36d74ba841e48863dc40
b3rian <brayann.8189@gmail.com>
2025-07-11 17:16:48 +0300

    edited

--- models/build_vgg_19.py
+++ models/build_vgg_19.py
@@ -1,22 +1,47 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
 
-def VGG19(input_shape=(224, 224, 3), num_classes=200):
-    model = models.Sequential()
+def simple_cnn_tiny_imagenet(input_shape=(64, 64, 3), num_classes=200):
+    model = models.Sequential([
+        # Layer 1
+        layers.Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=input_shape),
+        layers.BatchNormalization(),
+        
+        # Layer 2
+        layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
+        layers.MaxPooling2D((2, 2)),
 
-    # Block 1
-    model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=input_shape))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
-    model.add(layers.MaxPooling2D((2, 2), strides=2))
+        # Layer 3
+        layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
+        layers.BatchNormalization(),
 
-    # Block 2
-    model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
-    model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
-    model.add(layers.MaxPooling2D((2, 2), strides=2))
+        # Layer 4
+        layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
+        layers.MaxPooling2D((2, 2)),
 
-    # Block 3
-    model.add(layers.Conv2D(256, (3, 3), activation='relu', padding='same'))
-    model.add(layers.Conv2D(256, (3, 3), activation='relu', padding='same'))
-    mod 
+        # Layer 5
+        layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
+        layers.BatchNormalization(),
 
+        # Layer 6
+        layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
+        layers.MaxPooling2D((2, 2)),
+
+        # Flatten
+        layers.Flatten(),
+
+        # Layer 7 (Dense)
+        layers.Dense(512, activation='relu'),
+        layers.Dropout(0.5),
+
+        # Layer 8
+        layers.Dense(256, activation='relu'),
+
+        # Layer 9
+        layers.Dense(128, activation='relu'),
+
+        # Layer 10 - Output Layer
+        layers.Dense(num_classes, activation='softmax')
+    ])
+    
     return model
