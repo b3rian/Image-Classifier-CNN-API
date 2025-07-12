dae849e62557dc70c4a61dc2198dd232db6d4ed2
b3rian <brayann.8189@gmail.com>
2025-07-12 12:57:50 +0300

    edited

---
+++ app/api/endpoints.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter, UploadFile, File, HTTPException
+from fastapi.responses import JSONResponse
+from PIL import Image
+import numpy as np
+import io
+
+from app.predictor import predict
