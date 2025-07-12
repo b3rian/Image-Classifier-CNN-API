9a15222f189b0c9b279a6d4cba4230863acce4a4
b3rian <brayann.8189@gmail.com>
2025-07-12 14:44:41 +0300

    edited

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,6 +1,16 @@
-from fastapi import APIRouter, UploadFile, File, HTTPException
+import logging
+from fastapi import APIRouter, UploadFile, File, HTTPException, status
 from fastapi.responses import JSONResponse
-from PIL import Image
+from fastapi.requests import Request
+from PIL import Image, UnidentifiedImageError
+import numpy as np
+import io
+from typing import List, Dict
+from pydantic import BaseModel
+from datetime import datetime
+import os
+
+from app.predictor import predict
  
 
  
