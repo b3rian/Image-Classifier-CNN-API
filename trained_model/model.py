6c9bc87383a8e6da1cf548f67f1f1861527c199a
b3rian <brayann.8189@gmail.com>
2025-07-12 09:09:28 +0300

    edited

--- trained_model/model.py
+++ trained_model/model.py
@@ -1,3 +1,4 @@
+import tensorflow
 from tensorflow.keras.applications import EfficientNetV2L
 model = EfficientNetV2L(weights="imagenet")
-model.save("custome_cnn_model_1000_classes.keras")
+model.save("custom_cnn_model_1000_classes.keras")
