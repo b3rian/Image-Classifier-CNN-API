282dfabbb0029aa228c291876f2859dce1fc08d7
b3rian <brayann.8189@gmail.com>
2025-07-13 16:40:13 +0300

    commits

--- streamlit_app/cache_manager.py
+++ streamlit_app/cache_manager.py
@@ -1,6 +1,7 @@
 from functools import lru_cache
 import pandas as pd
 from config import config
+import streamlit as st
 
 class CacheManager:
     @staticmethod
@@ -9,3 +10,20 @@ class CacheManager:
         """Cache class labels"""
         from streamlit_app.api_client import APIClient
         return APIClient.get_class_labels()
+    
+    @staticmethod
+    def cache_prediction(image_hash: str, predictions: dict):
+        """Store prediction results"""
+        if not hasattr(st, 'session_state'):
+            return
+        
+        if 'predictions_cache' not in st.session_state:
+            st.session_state.predictions_cache = pd.DataFrame(
+                columns=['image_hash', 'predictions', 'timestamp']
+            )
+        
+        st.session_state.predictions_cache.loc[len(st.session_state.predictions_cache)] = {
+            'image_hash': image_hash,
+            'predictions': predictions,
+            'timestamp': pd.Timestamp.now()
+        }
