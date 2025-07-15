bac06d7f5d821f446bc35341c1cead21242ea5bd
b3rian <brayann.8189@gmail.com>
2025-07-15 11:48:32 +0300

    api commits

--- my_api/utils.py
+++ my_api/utils.py
@@ -1,12 +1,13 @@
 import tensorflow as tf
 
 # Loading the Keras model
-model = tf.keras.models.load_model("custom_model.keras")
+model = tf.keras.models.load_model("D:\Telegram Desktop\custom_cnn_model_1000_classes.keras")
 
 # Convert to TFLite with float16 quantization
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 converter.target_spec.supported_types = [tf.float16]
+tflite_model = converter.convert()
 
 # Save to desired path
 output_path = r"D:\Documents\Datasets\custom_model.tflite"
