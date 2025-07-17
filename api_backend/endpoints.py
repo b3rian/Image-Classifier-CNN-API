afaac8c0ddbdbde69b8aecb4e58b06cbb114fac6
b3rian <brayann.8189@gmail.com>
2025-07-17 09:41:49 +0300

    commits

--- api_backend/endpoints.py
+++ api_backend/endpoints.py
@@ -251,5 +251,4 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=8000,
         reload=True,
-        log_level=settings.log_level.lower()
-    )
+        log_level=settings.log_level.lower())
