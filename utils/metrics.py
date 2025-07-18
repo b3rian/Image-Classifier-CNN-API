503f2de1e97131649080de0f1c0875a1d379a4a5
b3rian <brayann.8189@gmail.com>
2025-07-18 16:49:50 +0300

    Clean branch to only include streamlit_app

--- utils/metrics.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-def get_classification_metrics():
-    return [
-        tf.keras.metrics.CategoricalAccuracy(name="accuracy"),
-        tf.keras.metrics.TopKCategoricalAccuracy(k=5, name="top5_accuracy")
-    ]
